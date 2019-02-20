006f3dbb44962e0d2d0bf137b4275a0bfef5416b
darshana27 <darshana27997@gmail.com>
2019-02-20 12:12:01 +0530

    Damn accesstoken issue solved

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,15 +10,15 @@ import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 export class HomePage implements OnInit {
   constructor(public api: ApiService) {}
   ngOnInit() {
-    var access_token = JSON.stringify(localStorage.getItem("access_token"));
-    console.log("Access Token : ", access_token);
+    // var access_token = JSON.stringify(localStorage.getItem("access_token"));
+    // console.log("Access Token : ", access_token);
+    var access_token = localStorage.getItem("access_token");
 
-    let headers = new HttpHeaders({
-      access_token: access_token
-    });
+    var headers = { headers: { access_token: access_token } };
+    console.log("Access Token : ", access_token);
     // let headers = new HttpHeaders();
     // headers = headers.append("access_token", access_token);
-    // console.log("***********", headers);
+    console.log("***********", headers);
     this.api.fetchApi(
       "http://staging.php-dev.in:8844/trainingapp/api/users/getUserData",
       "GET",
